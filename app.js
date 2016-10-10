603b01196ac84098341c2d492e09760a76663284
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-09 21:57:35 -0500

    bad oatuh error

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 // require npm modules
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
+var inquirer = require('inquirer');
+var keys = require('./keys.js')
 
 // twitter init
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.access_token_secret
 });
 
 var command = process.argv[2];
 var title = process.argv.slice(3).toString();
 
 var commands = {
-  my-tweets: function() {
-    
+  "my-tweets": function() {
+    // inquirer.prompt([
+    //   {
+    //     type: 'input',
+    //     message: 'Enter the screen name of the user whose tweets you want to read:',
+    //     name: 'userName'
+    //   }
+    // ]).then(function(input) {
+    // var screenName = input.userName;
+    // console.log(screenName);
+    client.get('favorites/list', {q: 'anything'}, function(error, tweets, response) {
+      console.log(tweets);
+    })
+  // })
   }
 }
+
+commands[command](title);
