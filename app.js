fef87136c9f79d13339152857440e50a81ed1b43
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-09 20:28:07 -0500

    require modules and twitter keys

---
+++ app.js
@@ -0,0 +1,12 @@
+// require npm modules
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+
+// twitter init
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
